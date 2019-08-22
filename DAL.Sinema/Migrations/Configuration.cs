c50af3b172db126cf936d7e08e617383e6a811a1
Serkan <Serkan@GS-1905>
2019-08-22 18:44:47 +0300

    Migrations eklendi, automatic migrations aktif edildi.

---
+++ DAL.Sinema/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace DAL.Sinema.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.Sinema.Context.SinemaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(DAL.Sinema.Context.SinemaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
