df7d7f3306934aa2d0851025415e91e9db47309e
Serkan <Serkan@GS-1905>
2019-08-22 21:04:52 +0300

    Repositoryler eklendi,repositoryservice ayarlandı.

---
+++ BLL.Sinema/Repository/Service/RepositoryService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Sinema.Repository.Service
+{
+    public class RepositoryService
+    {
+        private readonly CustomerRepository customerRepository;
+        private readonly HallRepository hallRepository;
+        private readonly MovieRepository movieRepository;
+        private readonly SeanceRepository seanceRepository;
+        private readonly SeatRepository seatRepository;
+        private readonly TicketRepository ticketRepository;
+        private readonly TicketDetailRepository ticketDetailRepository;
+        private readonly TicketDetailSeatRepository ticketDetailSeatRepository;
+
+
+        public RepositoryService()
+        {
+            customerRepository = new CustomerRepository();
+            hallRepository = new HallRepository();
+            movieRepository = new MovieRepository();
+            seanceRepository = new SeanceRepository();
+            seatRepository = new SeatRepository();
+            ticketRepository = new TicketRepository();
+            ticketDetailRepository = new TicketDetailRepository();
+            ticketDetailSeatRepository = new TicketDetailSeatRepository();
+        }
+        public CustomerRepository Customer => customerRepository;
+        public HallRepository Hall => hallRepository;
+        public MovieRepository Movie => movieRepository;
+        public SeanceRepository Seance => seanceRepository;
+        public SeatRepository Seat => seatRepository;
+        public TicketRepository Ticket => ticketRepository;
+        public TicketDetailRepository TicketDetail => ticketDetailRepository;
+        public TicketDetailSeatRepository TicketDetailSeat => ticketDetailSeatRepository;
+    }
+}
